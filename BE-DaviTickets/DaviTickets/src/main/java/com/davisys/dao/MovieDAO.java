eed98bc1f263957311ce4d6bbd9990389e558861
Dinhisme <phungquocvinh15112003@gmail.com>
2023-07-26 09:23:27 +0700

    Upload Database

--- BE-DaviTickets/DaviTickets/src/main/java/com/davisys/dao/MovieDAO.java
+++
@@ -1,9 +0,0 @@
-package com.davisys.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.davisys.entity.Movie;
-
-public interface MovieDAO extends JpaRepository<Movie, Integer>{
-
-}
--- BE-DaviTickets/DaviTickets/src/main/java/com/davisys/dao/ShowtimeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.davisys.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.davisys.entity.Showtime;
-
-public interface ShowtimeDAO extends JpaRepository<Showtime, Integer>{
-
-}
