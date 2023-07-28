100cdf54f7f3f036d5d7d04f69c6a120172de4d9
DoanSy <99658269+DoanSy16@users.noreply.github.com>
2023-07-28 23:25:56 +0700

    [sydh]show_listMovie

---
+++ DaviTickets/src/main/java/com/davisys/controller/AdminController.java
@@ -0,0 +1,109 @@
+package com.davisys.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.davisys.dao.MovieDAO;
+import com.davisys.dao.PaymentDAO;
+import com.davisys.dao.ShowtimeDAO;
+import com.davisys.dao.UserDAO;
+import com.davisys.entity.Movie;
+
+import jakarta.websocket.Session;
+
+@Controller
+public class AdminController {
+	@Autowired
+	PaymentDAO paymentDao;
+	@Autowired
+	UserDAO userDao;
+	@Autowired
+	ShowtimeDAO showtimeDao;
+	
+	Date now = new Date();
+	int day = Integer.valueOf(now.getDay());
+	int month = Integer.valueOf(now.getMonth());
+	
+	@GetMapping("/")
+	public String index(Model m) {
+		
+		loadRevenueStatisticsDay(m);
+		loadRevenueStatisticsMonth(m);
+		loadPercent(m);
+		loaduserStatisticsMonth(m);
+		top3Hour(m);
+		m.addAttribute("actived","active");
+		return "admin/dasboard";
+	}
+	
+	public void loadRevenueStatisticsDay(Model m) {
+		String revenueDay = paymentDao.revenueStatisticsDay(day);
+		if(revenueDay == null) {
+			revenueDay = "0";
+		}
+		m.addAttribute("revenueDay", revenueDay);
+	}
+	
+	public void loadRevenueStatisticsMonth(Model m) {
+		String revenueMonth = paymentDao.revenueStatisticsMonth(month);
+		if(revenueMonth == null) {
+			revenueMonth = "0";
+		}
+		m.addAttribute("revenueMonth", revenueMonth);
+	}
+	
+	public void loadPercent(Model m) {
+		String revenueDay = paymentDao.revenueStatisticsDay(day);
+		String maxRevenueMonth = paymentDao.revenueStatisticsMaxDay(month);
+		float percent = 0;
+		if(revenueDay == null || maxRevenueMonth == null) {
+			percent = 0;
+		}else {
+			float revenueToDay = Integer.valueOf(revenueDay);
+			float maxRevenueInMonth = Integer.valueOf(maxRevenueMonth);
+			percent = (revenueToDay / maxRevenueInMonth) * 100;
+		}
+		m.addAttribute("percent", percent);
+	}
+	
+	public void loaduserStatisticsMonth(Model m) {
+		String userStatisticsMonth = userDao.userStatisticsMonth(month);
+		if(userStatisticsMonth == null) {
+			userStatisticsMonth = "0";
+		}
+		m.addAttribute("userStatisticsMonth", userStatisticsMonth);
+	}
+	
+	public void top3Hour(Model model) {
+		String top1P = "", top2P = "", top3P = "";
+		List<Object[]> listTop3 = showtimeDao.getTop3GoldenHour();
+		int i = 0;
+		for (Object[] oj : listTop3) {
+			if (i == 0) {
+				top1P = String.valueOf(oj[0]);
+			}
+			if (i == 1) {
+				top2P = String.valueOf(oj[0]);
+			}
+			if (i == 2) {
+				top3P = String.valueOf(oj[0]);
+			}
+			i++;
+		}
+		model.addAttribute("top1", top1P);
+		model.addAttribute("top2", top2P);
+		model.addAttribute("top3", top3P);
+	}
+	
+	
+
+	@GetMapping("/error")
+	public String error(Model m) {
+		return "error";
+	}
+}
---
+++ DaviTickets/src/main/java/com/davisys/controller/UserController.java
@@ -0,0 +1,118 @@
+package com.davisys.controller;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.davisys.dao.UserDAO;
+import com.davisys.entity.Users;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class UserController {
+	@Autowired
+	UserDAO userDao;
+	@Autowired
+	HttpServletRequest request;
+	
+	String update = "";
+	String disable = "";
+
+	@GetMapping("/tablesusers")
+	public String tablesusers(Model m) {
+		testLoadData(m);
+		m.addAttribute("activeu","active");
+		return "admin/tablesusers";
+	}
+	@RequestMapping("/editUsers/{userid}")
+	public String editUser(Model m, @PathVariable("userid") int userid) {
+		Users user = userDao.findIdUsers(userid);
+		m.addAttribute("formuser", user);
+		if(update == "true") {
+			m.addAttribute("alertUpdate", "true");
+		}if(update == "false") {
+			m.addAttribute("alertUpdate", "false");
+		}
+		if(disable == "true") {
+			m.addAttribute("alertDisable", "true");
+		}if(disable == "false") {
+			m.addAttribute("alertDisable", "false");
+		}
+		update = "";
+		disable = "";
+		return "admin/formusers";
+	}
+	@PostMapping("/updateUsers/{userid}")
+	public String updateUser(Model m, @RequestParam("file") MultipartFile file,
+			@PathVariable("userid") int userid) {
+		try {
+			update = "true";
+			Users user = userDao.findIdUsers(userid);
+			String full_name = request.getParameter("full_name");
+			String gender = request.getParameter("gender");
+			String phone = request.getParameter("phone");
+			String user_birtday = request.getParameter("user_birtday");
+			String user_dayjoin = request.getParameter("user_dayjoin");
+			String pattern = "yyyy-MM-dd";
+			
+			user.setUserid(userid);
+			user.setFull_name(full_name);
+			user.setGender(gender);
+			user.setPhone(phone);
+			
+			DateFormat dateFormat = new SimpleDateFormat(pattern);
+			try {
+				Date birthdayDate = dateFormat.parse(user_birtday);
+				user.setUser_birtday(birthdayDate);
+				Date dayjoinDate = dateFormat.parse(user_dayjoin);
+				user.setUser_dayjoin(dayjoinDate);
+			} catch (Exception e) {
+				update = "false";
+				e.printStackTrace();
+			}
+
+			
+			userDao.saveAndFlush(user);
+			return "redirect:/editUsers/{userid}";
+		} catch (Exception e) {
+			System.out.println("errr: " + e);
+			update = "false";
+			throw e;
+		}
+
+	}
+	@PostMapping("/disableUsers/{userid}")
+	public String disableUser(Model m, @RequestParam("file") MultipartFile file,
+			@PathVariable("userid") int userid) {
+		try {
+			disable = "true";
+			Users user = userDao.findIdUsers(userid);
+			user.setAccount_status(false);
+			
+			userDao.saveAndFlush(user);
+			return "redirect:/editUsers/{userid}";
+		} catch (Exception e) {
+			System.out.println("errr: " + e);
+			disable = "false";
+			throw e;
+		}
+
+	}
+	public void testLoadData(Model model) {
+		List<Users> listUser = userDao.findAll();
+		model.addAttribute("listUser",listUser );
+	}
+	
+}
