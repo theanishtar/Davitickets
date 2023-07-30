28ea7b216ef616bac35ad1af0db38232580e9e7f
DoanSy <99658269+DoanSy16@users.noreply.github.com>
2023-07-30 10:28:01 +0700

    update rolelist

--- Davitickets.sql
+++ Davitickets.sql
@@ -49,6 +49,12 @@ CREATE TABLE seat(
 	available BIT NOT NULL --Ghế còn khả dụng không
 )
 
+CREATE TABLE roles(
+    role_id INT IDENTITY PRIMARY KEY NOT NULL,
+	role_name NVARCHAR(50) NOT NULL,
+	role_des NVARCHAR(max) NULL
+)
+
 --Người dùng
 CREATE TABLE users (
 	userid INT IDENTITY PRIMARY KEY NOT NULL,
@@ -63,7 +69,12 @@ CREATE TABLE users (
 	user_birtday DATE NULL,
 	user_dayjoin DATE NOT NULL,
 	gg_id NVARCHAR(MAX) NULL,
-	user_role BIT NOT NULL --Vai trò người dùng
+)
+
+CREATE TABLE user_role(
+	id INT IDENTITY PRIMARY KEY NOT NULL,
+	userid INT NOT NULL FOREIGN KEY REFERENCES users(userid), --id ng dung
+	role_id INT NOT NULL FOREIGN KEY REFERENCES roles(role_id)--Vai trò người dùng
 )
 
 
@@ -85,19 +96,19 @@ CREATE TABLE payment (
 )
 
 INSERT INTO movie VALUES 
-	(N'Quỷ Quyệt', CAST('2023-07-07' AS Date), 132, N'Hài, Hành Động, Hồi hộp, Tâm Lý ', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/quyquyet.jpg?alt=media&token=7bcfbeba-0511-4db9-97da-70d866f2aecb', 4.5, N'Patrick Wilson', N'Phim rất hay thể loại rất phù hợp mọi lứa tuổi '),
-	(N'Xứ Sở Các Nguyên Tố', CAST('2023-06-16' AS Date), 101, N'Hài, Hành Động, Kịch Tính', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/xusocacnguyento.jpg?alt=media&token=6a1b2b77-30b4-4939-bc94-254f9e9b58ab', 4.4, N'Peter Sohn', N'Phim hài nhưng không kém phần xúc động và sự kịch tính'),
-	(N'Tay Đua Kiệt Xuất', CAST('2023-07-10' AS Date), 128, N'Hài, Tình Cảm, Hành Động', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/tayduakietxuat.jpg?alt=media&token=5fd5e254-0fc7-4c7a-b94e-67c81c126a03', 4.0, N'Ross Venokur', N'Phim hài, rất xúc động tạo nên sự khác biệt của các thể loại tình cảm'),
-	(N'Quý Công Tử', CAST('2023-06-20' AS Date), 96, N'Hành Động, Hài', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/quycongtu.jpg?alt=media&token=edc06cdf-05c9-47b8-8c70-78025298061c', 4.2, N'Duy Joseph', N'Phim hành động pha trộn hài hước vui vẻ'),
-	(N'Đảo Tử Thần', CAST('2023-07-05' AS Date), 90, N'Hài, Hành Động', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/daotuthan.jpg?alt=media&token=1d29ffe8-cadf-4d9a-81ca-a28b0254927c', 4.6, N'Park Hoon-jung', N'Phim hành động xem kẻ hài hước '),
-	(N'Sid Toang Rồi', CAST('2023-07-12' AS Date), 95, N'Hài', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/sidtoangroi.jpg?alt=media&token=740d2f70-2341-45ff-9e5f-a78b96a5ab12', 4.0, N'Eiichirō Hasumi', N'Phim hài hước tạo nên sự vui vẻ '),
-	(N'RUBY Thủy Quái Tuổi Teen', CAST('2023-06-28' AS Date), 110, N'Hài, Tình Cảm', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/rubythuyquaituoiteen.jpg?alt=media&token=e99359bf-e462-4a55-b551-226d249e72f7', 4.7, N'Eli Gonda', N'Phim hài nhưng không kém phần xúc động và sự kịch tính'),
-	(N'LIVESTREAM', CAST('2023-04-28' AS Date), 100, N'Hài, Hành Động', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/livestream.jpg?alt=media&token=604d9d7d-a0fc-441b-aed2-36eecf0189d1', 4.6, N'Kirk DeMicco, Faryn Pearl', N'Phim hài hước, xen kẽ hành động'),
-	(N'Bỗng Dưng Được Yêu', CAST('2023-07-05' AS Date), 100, N'Hành Động, Hài Hước', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/bongdungduocyeu.jpg?alt=media&token=4d76643e-f71b-4adf-a530-5597f5453599', 4.1, N'Danny Đỗ', N'Phim hành động pha trộn hài hước vui vẻ'),
-	(N'FLASH', CAST('2023-06-14' AS Date), 120, N'Tâm Lý, Tình Cảm', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/flash.jpg?alt=media&token=32a81f64-63db-4b1b-9105-b6bdaea3ff63', 4.8, N'Trần Ngọc Giàu', N'Phim hài, rất xúc động tạo nên sự khác biệt của các thể loại tình cảm'),
-	(N'Ma Sơ Trục Quỷ', CAST('2023-06-28' AS Date), 101, N'Hài, Hành Động', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/masotrucquy.jpg?alt=media&token=1d336ae0-59e9-4783-9596-e24be10cfc5e', 4.4, N'Hoàng Tuấn Cường', N'Phim hành động pha trộn hài hước vui vẻ'),
-	(N'Cô Thành Trong Gương', CAST('2023-04-28' AS Date), 88, N'Hài, Tình Cảm', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/cothanhtrongguog.jpg?alt=media&token=172e6ad6-28eb-4903-ba94-4df2323d5592', 4.3, N'Đức Thịnh', N'Phim hài nhưng không kém phần xúc động và sự kịch tính'),
-	(N'Tà Chú Cấm', CAST('2023-06-21' AS Date), 87, N'Hài, Tình Cảm', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/tachucam.jpg?alt=media&token=96213f86-c1cc-4d1d-a3fe-1fa100a0f4f6', 4.6, N'Lê Lộc', N'Phim rất hay thể loại rất phù hợp mọi lứa tuổi')
+	(N'Quỷ Quyệt', CAST('2023-07-07' AS Date), 132, N'Hài, Hành Động, Hồi hộp, Tâm Lý ', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/quyquyet.jpg?alt=media&token=c939c324-5934-4e66-9450-3552f201f091', 4.5, N'Patrick Wilson', N'Phim rất hay thể loại rất phù hợp mọi lứa tuổi '),
+	(N'Xứ Sở Các Nguyên Tố', CAST('2023-06-16' AS Date), 101, N'Hài, Hành Động, Kịch Tính', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/xusocacnguyento.jpg?alt=media&token=e66a0c12-f74a-43cf-807b-8959be043da2', 4.4, N'Peter Sohn', N'Phim hài nhưng không kém phần xúc động và sự kịch tính'),
+	(N'Tay Đua Kiệt Xuất', CAST('2023-07-10' AS Date), 128, N'Hài, Tình Cảm, Hành Động', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/tayduakietxuat.jpg?alt=media&token=2254e7f8-529f-403e-ace5-a391021db6cc', 4.0, N'Ross Venokur', N'Phim hài, rất xúc động tạo nên sự khác biệt của các thể loại tình cảm'),
+	(N'Quý Công Tử', CAST('2023-06-20' AS Date), 96, N'Hành Động, Hài', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/quycongtu.jpg?alt=media&token=f76afc37-59e0-46f9-bdbe-8d43e707a18a', 4.2, N'Duy Joseph', N'Phim hành động pha trộn hài hước vui vẻ'),
+	(N'Đảo Tử Thần', CAST('2023-07-05' AS Date), 90, N'Hài, Hành Động', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/daotuthan.jpg?alt=media&token=592559f3-9fe7-42e9-8326-50b0d36477b5', 4.6, N'Park Hoon-jung', N'Phim hành động xem kẻ hài hước '),
+	(N'Sid Toang Rồi', CAST('2023-07-12' AS Date), 95, N'Hài', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/sidtoangroi.jpg?alt=media&token=ba3aef62-fed0-4e6f-bc8a-d5d793cefbc4', 4.0, N'Eiichirō Hasumi', N'Phim hài hước tạo nên sự vui vẻ '),
+	(N'RUBY Thủy Quái Tuổi Teen', CAST('2023-06-28' AS Date), 110, N'Hài, Tình Cảm', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/rubythuyquaituoiteen.jpg?alt=media&token=42304cf7-5e5f-40de-94ee-f6e3aaed0ba2', 4.7, N'Eli Gonda', N'Phim hài nhưng không kém phần xúc động và sự kịch tính'),
+	(N'LIVESTREAM', CAST('2023-04-28' AS Date), 100, N'Hài, Hành Động', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/livestream.jpg?alt=media&token=e3c9064e-b229-41aa-8960-dfa22c02dae4', 4.6, N'Kirk DeMicco, Faryn Pearl', N'Phim hài hước, xen kẽ hành động'),
+	(N'Bỗng Dưng Được Yêu', CAST('2023-07-05' AS Date), 100, N'Hành Động, Hài Hước', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/bongdungduocyeu.jpg?alt=media&token=4a71b72a-f725-4a37-ace4-ddeacc94664b', 4.1, N'Danny Đỗ', N'Phim hành động pha trộn hài hước vui vẻ'),
+	(N'FLASH', CAST('2023-06-14' AS Date), 120, N'Tâm Lý, Tình Cảm', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/flash.jpg?alt=media&token=bf515c1d-a1cf-4e01-bb42-319bc7e5cbca', 4.8, N'Trần Ngọc Giàu', N'Phim hài, rất xúc động tạo nên sự khác biệt của các thể loại tình cảm'),
+	(N'Ma Sơ Trục Quỷ', CAST('2023-06-28' AS Date), 101, N'Hài, Hành Động', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/masotrucquy.jpg?alt=media&token=d7c10aa7-86ba-4b07-984c-28cbebc4755a', 4.4, N'Hoàng Tuấn Cường', N'Phim hành động pha trộn hài hước vui vẻ'),
+	(N'Cô Thành Trong Gương', CAST('2023-04-28' AS Date), 88, N'Hài, Tình Cảm', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/cothanhtrongguog.jpg?alt=media&token=dde6b93a-2d47-452b-adc5-f2af93d82d77', 4.3, N'Đức Thịnh', N'Phim hài nhưng không kém phần xúc động và sự kịch tính'),
+	(N'Tà Chú Cấm', CAST('2023-06-21' AS Date), 87, N'Hài, Tình Cảm', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/tachucam.jpg?alt=media&token=a95376bd-1c26-4602-89f4-01048a02f55c', 4.6, N'Lê Lộc', N'Phim rất hay thể loại rất phù hợp mọi lứa tuổi')
 
 INSERT INTO room VALUES
 	(1, N'Phòng chiếu 1'),
@@ -201,14 +212,27 @@ INSERT INTO seat VALUES
 	(0, 'G', 10, 1),
 	(0, 'G', 11, 1)
 
+INSERT INTO roles VALUES
+	('ROLE_ADMIN',N'Quản trị web'),
+	('ROLE_USER',N'Người dùng')
+
 INSERT INTO users VALUES 
-	(N'Trần Hữu Đang', 'Nam', N'dangth', N'0917288723', N'dangthpc04349@fpt.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/dangth.jpg?alt=media&token=2c78efe0-94eb-4d5e-8cd3-fe5832a5652d', 1, 1, CAST('9-7-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL, 1),
-	(N'Lê Bích Vi', 'Nữ', N'vilb', N'0178296424', N'vilbpc04354@fpt.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/vilb.jpg?alt=media&token=c3f42325-5d1d-44e8-9f44-9b645c4db9bb', 1, 1, CAST('6-2-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL, 0),
-	(N'Phùng Quốc Vinh', 'Nam', N'vinhpq', N'0862738927', N'vinhpqpc04338@fpt.edu.vn',  'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/thaynghia.jpg?alt=media&token=193330be-4f6e-48ac-9890-84fed6ea2667', 1, 1, CAST('11-15-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL, 0),
-	(N'Đoàn Hiệp Sỹ', 'Nam', N'sydh', N'0836452473', N'sydhpc04388@fpt.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/sydh.jpg?alt=media&token=5d7a0508-7e8f-4c18-a6b1-dc7999c3bb92', 1, 1, CAST('4-7-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL, 0),
-	(N'Nguyễn Khánh Đan', 'Nữ', N'dannk', N'0924637483', N'dannkpc04351@fpt.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/dannk.jpg?alt=media&token=83fcc574-b6f8-4f50-85df-3aab6be31aa8', 1, 1, CAST('11-7-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL, 0),
-	(N'Châu Hoài Phúc', 'Nam', N'phucch', N'0918093162', N'phucchpc04191@fpt.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/phucch.jpg?alt=media&token=0aadaab5-b399-41b3-bfd1-aded2d52777a', 1, 1, CAST('11-2-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL, 0),
-	(N'Quách Hữu Nghĩa', 'Nam', N'nghiahq', N'012346789', N'nghiaqh@fe.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-33803.appspot.com/o/vinhpq.jpg?alt=media&token=055b0359-9e5b-4213-a4da-7000bd2f09de', 1, 1, CAST('1-1-1990' AS DATE), CAST('7-22-2023' AS DATE), NULL, 0)
+	(N'Trần Hữu Đang', 'Nam', N'dangth', N'0917288723', N'dangthpc04349@fpt.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/dangth.jpg?alt=media&token=e223770c-06cb-448e-9025-43000f55d764', 1, 1, CAST('9-7-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL),
+	(N'Lê Bích Vi', 'Nữ', N'vilb', N'0178296424', N'vilbpc04354@fpt.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/vilb.jpg?alt=media&token=83641b31-7ea9-432d-bd6b-4dd0f5e9062f', 1, 1, CAST('6-2-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL),
+	(N'Phùng Quốc Vinh', 'Nam', N'vinhpq', N'0862738927', N'vinhpqpc04338@fpt.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/vinhpq.jpg?alt=media&token=635b97b6-bdf4-49b5-ae07-a802c17a979e', 1, 1, CAST('11-15-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL),
+	(N'Đoàn Hiệp Sỹ', 'Nam', N'sydh', N'0836452473', N'sydhpc04388@fpt.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/sydh.jpg?alt=media&token=f907c8e9-4712-4448-b7a9-1d9df8f9b053', 1, 1, CAST('4-7-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL),
+	(N'Nguyễn Khánh Đan', 'Nữ', N'dannk', N'0924637483', N'dannkpc04351@fpt.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/dannk.jpg?alt=media&token=2cb34557-c380-4095-8a10-8a211add0940', 1, 1, CAST('11-7-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL),
+	(N'Châu Hoài Phúc', 'Nam', N'phucch', N'0918093162', N'phucchpc04191@fpt.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/phucch.jpg?alt=media&token=8ee61c10-23b1-41a5-97ed-b1e0e6d894ed', 1, 1, CAST('11-2-2003' AS DATE), CAST('7-22-2023' AS DATE), NULL),
+	(N'Quách Hữu Nghĩa', 'Nam', N'nghiahq', N'012346789', N'nghiaqh@fe.edu.vn', 'https://firebasestorage.googleapis.com/v0/b/davitickets-2e627.appspot.com/o/thaynghia.jpg?alt=media&token=9fc95aed-1dfe-4b87-8ebe-2903ffd50678', 1, 1, CAST('1-1-1990' AS DATE), CAST('7-22-2023' AS DATE), NULL)
+
+INSERT INTO user_role VALUES
+	(1,1),
+	(2 ,2),
+	(3 ,2),
+	(4 ,2),
+	(5 ,2),
+	(6 ,2),
+	(7 ,1)
 
 INSERT INTO booking VALUES 
 	(1, 27, 1, CAST('2023-06-17' AS Date)),
@@ -307,3 +331,4 @@ SELECT * FROM booking INNER JOIN payment ON payment.booking_id = booking.booking
 */
 
 
+SELECT *from roles
