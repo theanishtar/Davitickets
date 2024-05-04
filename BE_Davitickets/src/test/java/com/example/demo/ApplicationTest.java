245839521d8634d84466d32933e6b8f4cadd710b
theanishtar <dangtt135@gmail.com>
2024-05-04 10:08:20 +0700

    ini: docker file

---
+++ BE_Davitickets/src/test/java/com/example/demo/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ApplicationTest.class)
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
